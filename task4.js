417270acefb3097ac3bbf994f136dcaf2f67bc6c
AIDAI <turdakunovaidai@gmail.com>
2023-10-24 21:01:39 +0600

    sth

---
+++ task4.js
@@ -0,0 +1,53 @@
+/* 1. Напишите скрипт, который считает количество секунд в часе.*/
+
+const secondsInMinute = 60;
+const minutesInHour = 60;
+const secondsInHour = secondsInMinute * minutesInHour;
+
+console.log('Количество секунд в часе: ' + secondsInHour);
+
+
+/* 2. Создайте три переменные - час, минута, секунда. 
+С их помощью выведите текущее время в формате 'час:минута:секунда'.*/
+
+// const currentDate = new Date();
+
+// const hour = currentDate.getHours();
+// const minute = currentDate.getMinutes();
+// const second = currentDate.getSeconds();
+// const formattedTime = hour + ':' + minute + ':' + second;
+// console.log('Текущее время: ' + formattedTime);
+
+
+
+
+ /* 3. В переменной day лежит какое-то число из интервала от 1 до 31. 
+ Определите в какую декаду месяца попадает это число (в первую, вторую или третью).*/
+
+// let day = 17; 
+
+// if (day >= 1 && day <= 10) {
+//     console.log ("первыи")
+// } else if (day >= 10 && day <= 20){
+//     console.log ("второи")
+// } else if (day >= 20 && day <= 31) {
+//     console.log ("третии")
+// }
+
+
+ /* 4. В переменной month лежит какое-то число из интервала от 1 до 12. 
+ Определите в какую пору года попадает этот месяц (зима, лето, весна, осень).*/
+ 
+// let month = 7; 
+
+// if (month >= 1 && month <= 3) {
+//     console.log("Это зима");
+// } else if (month >= 4 && month <= 6) {
+//     console.log("Это весна");
+// } else if (month >= 7 && month <= 9) {
+//     console.log("Это лето");
+// } else if (month >= 10 && month <= 12) {
+//     console.log("Это осень");
+// } else {
+//     console.log("Некорректно");
+// }
